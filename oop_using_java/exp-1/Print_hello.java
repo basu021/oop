5f9ca42bdaa428dcbec4c1f4041956fd2e77d6b8
Abinash Patra <patraabinash1028@gmail.com>
2022-02-11 15:49:45 +0530

    Update Print_hello.java

--- oop_using_java/exp-1/Print_hello.java
+++ oop_using_java/exp-1/Print_hello.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Print_hello {
     public static void main(String[] args) {
+        String Your_Name;
+        Scanner in = new Scanner(System.in);
+        Your_Name = in.nextLine();
+        in.close();
         System.out.println("Hello");
-        System.out.println("Your_Name");
+        System.out.println(Your_Name);
     }
 }
